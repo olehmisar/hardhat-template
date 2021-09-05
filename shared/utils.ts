e1120ec5cea58052e25986cc8d1a379ecd2b2ceb
Oleh Misarosh <olehmisar@gmail.com>
2021-09-05 18:50:05 +0300

    Add common utils

---
+++ shared/utils.ts
@@ -0,0 +1,36 @@
+import { ethers } from 'hardhat';
+
+export async function getBlockTimestamp() {
+  return (await ethers.provider.getBlock('latest')).timestamp;
+}
+
+export async function evmIncreaseTime(offset: number) {
+  await ethers.provider.send('evm_mine', [(await getBlockTimestamp()) + offset]);
+}
+
+const snapshots: string[] = [];
+/**
+ * Runs `fn` once, saves EVM state and restores it before each tests.
+ */
+export function snapshottedBeforeEach(fn: () => Promise<void>) {
+  before(async () => {
+    snapshots.push(await ethers.provider.send('evm_snapshot', []));
+    await fn();
+  });
+
+  beforeEach(async () => {
+    snapshots.push(await ethers.provider.send('evm_snapshot', []));
+  });
+
+  afterEach(async () => {
+    if (!(await ethers.provider.send('evm_revert', [snapshots.pop()]))) {
+      throw new Error('evm_revert failed');
+    }
+  });
+
+  after(async () => {
+    if (!(await ethers.provider.send('evm_revert', [snapshots.pop()]))) {
+      throw new Error('evm_revert failed');
+    }
+  });
+}
